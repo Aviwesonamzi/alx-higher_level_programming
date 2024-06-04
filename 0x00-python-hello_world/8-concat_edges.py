4476f2a45c4fce4fca2cd769593be32da83443d5
root <you@example.com>
2024-06-04 05:59:11 -0700

    8. Create a new sentence

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(str[str.find("object-oriented programming"):])
+str = str[39:67] + str[-23:-18] + " " + str[:7]
+print(str)
