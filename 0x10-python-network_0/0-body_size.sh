6618883e0bf63241798f85c750d49e8b31ecc0e3
root <you@example.com>
2024-07-04 15:44:50 -0700

    network

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 # This script takes in a URL, sends a request to that URL, and displays the size of the body of the response in bytes
+curl -s -o /dev/null -w "%{size_download}\n" "$1"
 
-if [ -z "$1" ]; then
-  echo "Usage: $0 <URL>"
-  exit 1
-fi
-
-url=$1
-curl -s -o /dev/null -w "%{size_download}\n" "$url"
