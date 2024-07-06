11963dde50e51068e4e20c636b723e409e62480d
root <you@example.com>
2024-07-05 21:46:07 -0700

    bash

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# This script takes in a URL, sends a request to that URL, and displays the size of the body of the response in bytes
-curl -s -o /dev/null -w "%{size_download}\n" "$1"
-
+# Sends a request to a URL and displays the size of the response body in bytes
+curl -s "$1" | wc -c
