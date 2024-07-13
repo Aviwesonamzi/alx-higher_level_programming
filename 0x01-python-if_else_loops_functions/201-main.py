9698230b416519f8e4b55599767dafb82ae6469e
root <you@example.com>
2024-07-13 16:57:58 -0700

    magic

---
+++ 0x01-python-if_else_loops_functions/201-main.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+magic_calculation = __import__('102-magic_calculation').magic_calculation
+
+print(magic_calculation(1, 2, 3))  # Expected output: 3
+print(magic_calculation(3, 1, 2))  # Expected output: 4
+print(magic_calculation(3, 2, 1))  # Expected output: 5
