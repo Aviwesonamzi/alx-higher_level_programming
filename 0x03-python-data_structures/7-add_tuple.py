469dea578018511da42645179ada6550788c3fbb
root <you@example.com>
2024-07-24 06:38:31 -0700

    tuple

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    # Ensure tuples have at least 2 elements by appending zeros and then slicing
+# Ensure tuples have at least 2 elements by appending zeros and then slicing
     a1, a2 = (tuple_a + (0, 0))[:2]
     b1, b2 = (tuple_b + (0, 0))[:2]
     # Sum corresponding elements
