b770e048646e69d5105a2748c615cca9d35d0f2c
root <you@example.com>
2024-07-31 16:14:26 -0700

    tasks

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+const request = require('request');
+const movieId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error('error:', error);
+  } else {
+    const characters = JSON.parse(body).characters;
+    characters.forEach(characterUrl => {
+      request(characterUrl, (error, response, body) => {
+        if (error) {
+          console.error('error:', error);
+        } else {
+          const characterName = JSON.parse(body).name;
+          console.log(characterName);
+        }
+      });
+    });
+  }
+});
