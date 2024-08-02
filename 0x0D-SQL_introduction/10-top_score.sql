3fed0c5b2362d85bcabcf70e75dfaa4456bc1051
root <you@example.com>
2024-08-02 12:45:35 -0700

    DESC

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,5 @@
+-- List all records from second_table of the database hbtn_0c_0
+-- Display both the score and the name, ordered by score (top first)
+SELECT score, name 
+FROM second_table 
+ORDER BY score DESC;
